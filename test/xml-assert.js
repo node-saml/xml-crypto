f8160597c01a1bdba1c5d19e5be97b694a2896c7
Henri Bergius <henri.bergius@iki.fi>
2012-07-03 14:48:32 +0200

    Don't run this as test directly

--- test/xml-assert.js
+++ test/xml-assert.js
@@ -3,6 +3,10 @@ var select = require('../lib/xpath.js').SelectNodes
 
 
 function nodeExists(test, doc, xpath) {
+  if (!doc && !xpath) {
+    test.done();
+    return;
+  }
   var node = select(doc, xpath)
   test.ok(node.length==1, "xpath " + xpath + " not found")  
 }
