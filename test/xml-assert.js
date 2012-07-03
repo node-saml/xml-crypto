6c3633cd57e88efc143415017e05175352ee7017
Yaron Naveh <yaronn01@gmail.com>
2012-07-03 21:49:02 +0300

    enveloped signature

--- test/xml-assert.js
+++ test/xml-assert.js
@@ -2,11 +2,8 @@ var select = require('../lib/xpath.js').SelectNodes
   , dom = require('xmldom').DOMParser
 
 
-function nodeExists(test, doc, xpath) {
-  if (!doc && !xpath) {
-    test.done();
-    return;
-  }
+function nodeExists(test, doc, xpath) {  
+  if (!doc && !xpath) return
   var node = select(doc, xpath)
   test.ok(node.length==1, "xpath " + xpath + " not found")  
 }
