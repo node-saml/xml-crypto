1e768ad6aeb20fe03a54e438508a6f81c2e87fd0
Kevin Horvatin <kevin.horvatin@software.dell.com>
2015-01-30 09:02:58 -0600

    Fixes issue where the data from Comments were written to the canonicalized string. Thanks to deoxxa/xml-c14n for the render comment code. Added out of the box support for the c-14-n#WithComments algorithm, this helps fix an issue when dealing with SAML responses from KeyCloak

--- test/saml-response-test.js
+++ test/saml-response-test.js
@@ -25,3 +25,16 @@ exports['test validating SAML response where a namespace is defined outside the 
   test.equal(result, true);
   test.done();
 };
+
+exports['test validating SAML response WithComments'] = function (test) {
+  var xml = fs.readFileSync('./test/static/valid_saml_withcomments.xml', 'utf-8');
+  var doc = new xmldom.DOMParser().parseFromString(xml);
+  var signature = crypto.xpath(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
+  var sig = new crypto.SignedXml();
+  sig.keyInfoProvider = new crypto.FileKeyInfo("./test/static/feide_public.pem");
+  sig.loadSignature(signature.toString());
+  var result = sig.checkSignature(xml);
+  // This doesn't matter, just want to make sure that we don't fail due to unknown algorithm
+  test.equal(result, false);
+  test.done();
+};
