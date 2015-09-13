d1ddec98eadd78b9f74a0c1aa91223117efe966e
Frank Cornelis <info@e-contract.be>
2015-09-13 14:39:14 +0200

    HMAC signature creation

--- test/hmac-tests.js
+++ test/hmac-tests.js
@@ -25,3 +25,27 @@ exports['test HMAC signature with incorrect key'] = function (test) {
     test.equal(result, false);
     test.done();
 };
+
+
+exports['test create and validate HMAC signature'] = function (test) {
+    var xml = "<library>" +
+            "<book>" +
+            "<name>Harry Potter</name>" +
+            "</book>" +
+            "</library>";
+    var sig = new crypto.SignedXml();
+    sig.signingKey = fs.readFileSync("./test/static/hmac.key");
+    sig.signatureAlgorithm = "http://www.w3.org/2000/09/xmldsig#hmac-sha1";
+    sig.addReference("//*[local-name(.)='book']");
+    sig.computeSignature(xml);
+
+    var doc = new xmldom.DOMParser().parseFromString(sig.getSignedXml());
+    var signature = crypto.xpath(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0];
+    var verify = new crypto.SignedXml();
+    verify.keyInfoProvider = new crypto.FileKeyInfo("./test/static/hmac.key");
+    verify.loadSignature(signature);
+    var result = verify.checkSignature(sig.getSignedXml());
+    test.equal(result, true);
+
+    test.done();
+};
