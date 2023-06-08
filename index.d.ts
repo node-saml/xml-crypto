4e631d5e13018529f9ae0045078e5b9b2588678f
Ivan Novak <admin@clab.hr>
2023-06-08 13:56:03 +0200

    prettier

--- index.d.ts
+++ index.d.ts
@@ -7,12 +7,14 @@
 
 import { SelectedValue } from "xpath";
 
-type CanonicalizationAlgorithmType = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+type CanonicalizationAlgorithmType =
+  | "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
   | "http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments"
   | "http://www.w3.org/2001/10/xml-exc-c14n#"
   | "http://www.w3.org/2001/10/xml-exc-c14n#WithComments";
 
-type TransformAlgorithmType = CanonicalizationAlgorithmType
+type TransformAlgorithmType =
+  | CanonicalizationAlgorithmType
   | "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
   | string;
 
