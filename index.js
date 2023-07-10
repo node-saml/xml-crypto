01242f2a33400ecf6d18ea917aa1bcc6adaaed5b
angeljqv <angel_jqv@hotmail.com>
2023-07-10 08:47:15 -0500

    Exports C14nCanonicalization, ExclusiveCanonicalization

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const select = require("xpath").select;
 const utils = require("./lib/utils");
 
 module.exports = require("./lib/signed-xml");
+module.exports.C14nCanonicalization = require("./lib/c14n-canonicalization").C14nCanonicalization;
+module.exports.C14nCanonicalizationWithComments =
+  require("./lib/c14n-canonicalization").C14nCanonicalizationWithComments;
+module.exports.ExclusiveCanonicalization =
+  require("./lib/exclusive-canonicalization").ExclusiveCanonicalization;
+module.exports.ExclusiveCanonicalizationWithComments =
+  require("./lib/exclusive-canonicalization").ExclusiveCanonicalizationWithComments;
 module.exports.xpath = function (node, xpath) {
   return select(xpath, node);
 };
