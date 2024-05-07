80c4a5b8762547ae50c7e8234cb1bd8b6b995fd1
parallels999 <109294935+parallels999@users.noreply.github.com>
2024-05-07 12:11:55 -0500

    Exports C14nCanonicalization, ExclusiveCanonicalizatio

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { SignedXml } from "./signed-xml";
+export { C14nCanonicalization, C14nCanonicalizationWithComments } from "./c14n-canonicalization";
+export { ExclusiveCanonicalization, ExclusiveCanonicalizationWithComments } from "./exclusive-canonicalization";
 export * from "./utils";
 export * from "./types";
