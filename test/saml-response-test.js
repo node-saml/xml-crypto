28f92218ecbb8dcbd238afa4efbbd50302aa9aed
Matt Dzwonczyk <9063128+mattgd@users.noreply.github.com>
2025-03-14 11:32:37 -0400

    Merge commit from fork
    * - Draft: see what is signed
    
    * Fix bugs
    
    * Fix bugs
    
    * Revert "Fix bugs"
    
    This reverts commit cd4b7d6fcb21cd1a40e57fd214c950c47c8667d5.
    
    * Revert "Fix bugs"
    
    This reverts commit 0947a89a04e16bedffc3cc5bf3152c6dedbe3a7d.
    
    * Revert "- Draft: see what is signed"
    
    This reverts commit 2a9c405f09f7843b994820f6933408d522a6d849.
    
    * Patch for version 3
    
    * Only parse references from canonicalized signed info
    
    * Space
    
    * Fixing spaces
    
    * remove space
    
    * Remove fix that does not apply
    
    * Formatting
    
    * Add warning about references.
    
    * Remove formatting changes.
    
    * Backport references, SignedInfo, and DigestValue patches.
    
    * Only allow exclusive canon.
    
    * Apply changes from v6 review
    
    * Revert package-lock.json
    
    * Apply suggested changes
    
    ---------
    
    Co-authored-by: SecureSAML <alex@securesaml.com>
    Co-authored-by: Blair Weber <blair@workos.com>

--- test/saml-response-test.js
+++ test/saml-response-test.js
@@ -93,3 +93,76 @@ exports["test validating SAML response WithComments"] = function (test) {
   test.equal(result, false);
   test.done();
 };
+
+exports["test validating SAML response with digest comment"] = function (test) {
+  var xml = fs.readFileSync("./test/static/valid_saml_with_digest_comment.xml", "utf-8");
+  var doc = new xmldom.DOMParser().parseFromString(xml);
+  const assertion = xpath.select1("//*[local-name(.)='Assertion']", doc);
+  const signature = xpath.select1(
+    "//*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']",
+    assertion,
+  );
+  var sig = new crypto.SignedXml();
+  sig.keyInfoProvider = new crypto.FileKeyInfo("./test/static/feide_public.pem");
+  sig.loadSignature(signature);
+  var result = sig.checkSignature(xml);
+  test.equal(sig.references[0].digestValue, "RnNjoyUguwze5w2R+cboyTHlkQk=");
+  test.equal(result, false);
+  test.done();
+};
+
+exports["test signature contains a `SignedInfo` node"] = function (test) {
+  var xml = fs.readFileSync("./test/static/invalid_saml_no_signed_info.xml", "utf-8");
+  var doc = new xmldom.DOMParser().parseFromString(xml);
+  const node = xpath.select1(
+    "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']",
+    doc,
+  );
+  var sig = new crypto.SignedXml();
+  sig.keyInfoProvider = new crypto.FileKeyInfo("./test/static/feide_public.pem");
+  test.throws(
+    function () {
+      sig.loadSignature(node);
+    },
+    Error,
+    "no signed info node found"
+  );
+  test.done();
+};
+
+exports["test validation ignores an additional wrapped `SignedInfo` node"] = function (test) {
+  var xml = fs.readFileSync("./test/static/saml_wrapped_signed_info_node.xml", "utf-8");
+  var doc = new xmldom.DOMParser().parseFromString(xml);
+  var assertion = xpath.select("//*[local-name(.)='Assertion']", doc)[0];
+  var signature = xpath.select(
+    "//*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']",
+    assertion
+  )[0];
+  var sig = new crypto.SignedXml();
+  sig.keyInfoProvider = new crypto.FileKeyInfo("./test/static/saml_external_ns.pem");
+  sig.loadSignature(signature);
+  test.equal(sig.references.length, 1);
+  var result = sig.checkSignature(xml);
+  test.equal(result, true);
+  test.done();
+};
+
+exports["test signature does not contain multiple `SignedInfo` nodes"] = function (test) {
+  var xml = fs.readFileSync("./test/static/saml_multiple_signed_info_nodes.xml", "utf-8");
+  var doc = new xmldom.DOMParser().parseFromString(xml);
+  var assertion = xpath.select("//*[local-name(.)='Assertion']", doc)[0];
+  var signature = xpath.select(
+    "//*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']",
+    assertion
+  )[0];
+  var sig = new crypto.SignedXml();
+  sig.keyInfoProvider = new crypto.FileKeyInfo("./test/static/saml_external_ns.pem");
+  test.throws(
+    function () {
+      sig.loadSignature(signature);
+    },
+    Error,
+    "could not load signature that contains multiple SignedInfo nodes"
+  );
+  test.done();
+};
