9ac1547bed50af0b413d82599e8d0d31063c1e33
Chris Barth <chrisjbarth@hotmail.com>
2025-04-21 10:11:33 -0500

    fix spelling

--- src/signed-xml.ts
+++ src/signed-xml.ts
@@ -323,7 +323,7 @@ export class SignedXml {
          valid(ated). Put simply: if one fails, they are all not trustworthy.
       */
       this.signedReferences = [];
-      // TODO: add this break change here later on for even more security: `this.references = [];`
+      // TODO: add this breaking change here later on for even more security: `this.references = [];`
       if (callback) {
         callback(new Error("Could not validate all references"), false);
         return;
@@ -358,7 +358,7 @@ export class SignedXml {
       // but that may cause some breaking changes, so we'll handle that in v7.x.
       // If we were validating `signedInfoCanon` first, we wouldn't have to reset this array.
       this.signedReferences = [];
-      // TODO: add this break change here later on for even more security: `this.references = [];`
+      // TODO: add this breaking change here later on for even more security: `this.references = [];`
 
       if (callback) {
         callback(
