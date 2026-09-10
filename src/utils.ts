c51742cf42507f91737ec4deaba607570a776924
Chris Barth <chrisjbarth@hotmail.com>
2026-09-10 13:53:16 -0500

    chore: deprecate the internal helpers 7.0 withdraws (#567)

--- src/utils.ts
+++ src/utils.ts
@@ -47,7 +47,10 @@ export function findChildren(node: Node | Document, localName: string, namespace
   return res;
 }
 
-/** @deprecated */
+/**
+ * @deprecated Will be removed in 7.0. This is an internal DOM helper with no replacement; use a
+ *   DOM API or the `xpath` package.
+ */
 export function findChilds(node: Node | Document, localName: string, namespace?: string) {
   return findChildren(node, localName, namespace);
 }
